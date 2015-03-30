12c4288c816c3924bd741ac50f36735d0fd98146
Python <diegorscorpion@gmail.com>
2015-03-30 16:49:55 -0600

    Declaración de compas
    Se declaró el compas y se aplico que debe empezar en 0

--- Jugador.java
+++ Jugador.java
@@ -6,12 +6,17 @@ import lejos.nxt.*;
 
 public class Jugador
 {
+	
 	public static void main(String[] args)
 	{
-		ColorSensor color=new ColorSensor(SensorPort.S2);
-		IRSeekerV2 infra=new IRSeekerV2(SensorPort.S1 ,IRSeekerV2.Mode.AC);
-		Movimientos m=new Movimientos();
- 		Pelota buscar= new Pelota(infra,m,color);
+		ColorSensor color = new ColorSensor(SensorPort.S2);
+		IRSeekerV2 infra = new IRSeekerV2(SensorPort.S1 ,IRSeekerV2.Mode.AC);
+		CompassHTSensor brujula = new CompassHTSensor(SensorPort.S3);
+		Movimientos m = new Movimientos();
+ 		Pelota buscar = new Pelota(infra, m, color);
+		
 		buscar.buscaPelota();
+
 	}
+
 }
--- Pelota.java
+++ Pelota.java
@@ -1,105 +1,150 @@
 import lejos.nxt.addon.*;
 import lejos.nxt.*;
 
-public class Pelota{
+public class Pelota
+{
     
    	private ColorSensor color;
     private IRSeekerV2 infra;
+    private CompassHTSensor brujula;
     private Movimientos m;
-    private int direccion;
-    public IRSeekerV2 getInfra(){  return infra;  }
+    private int direccion, orientacion;
+    
+    public IRSeekerV2 getInfra()
+    { 
+    
+    	return infra;  
+
+    }
     
 
-    public Pelota(IRSeekerV2 infrarrojo, Movimientos m, ColorSensor c){
-    	color=c;
+    public Pelota(IRSeekerV2 infrarrojo, CompassHTSensor compas, Movimientos m, ColorSensor c)
+    {
+    
+    	color = c;
 		infra = infrarrojo;
+		brujula = compas;
 		this.m = m;
 			 	  
     }
 
-    public void buscaPelota(){
+    public void buscaPelota()
+    {
 		
-		while( !Button.ESCAPE.isDown() ){
-
-
+		while(!Button.ESCAPE.isDown())
+		{
 
 			LCD.drawInt(color.getColorID(),1,1);
 			direccion = infra.getDirection();
-			LCD.drawInt(direccion,1,3);
+			LCD.drawInt(direccion, 1,3);
+			orientacion.resetCartesianZero();
+			LCD.drawInt(orientacion, 1,5);
 
-					 
-		    if (color.getColorID() == 1)
+
+			if (color.getColorID() == 1)
 		    {
+			    
 			    m.detenerAB();
 			    m.retrocederAB(900);
-			    try{
+			    
+			    try
+			    {
+				
 					Thread.sleep(900);
+				
 				}
-				catch(Exception e){}
-		    }
-		    else{
-
-            if (direccion==0){
-            	m.detenerA();
-            	m.frontCbackB(900);
-            }
+				
+				catch (Exception e){}
 		    
-			if( direccion == 1 ){
-				m.detenerB();
-				m.frontCbackA(300);
-			}
- 
-			if( direccion == 2 ){
-				m.detenerC();
-				m.frontBbackA(400);
-				
-			}
-         
-			if( direccion == 3 ){
-				m.detenerC();
-				m.frontBbackA(600);
-				
-			}
+		    }
+		    
+		    else
+		    {
 
-			if( direccion == 4 ){
-				m.detenerC();
-				m.frontBbackA(800);
+            	if (direccion == 0)
+            	{
+            
+            		m.detenerA();
+            		m.frontCbackB(900);
+            
+            	}
+		    
+				if (direccion == 1)
+				{
+			
+					m.detenerB();
+					m.frontCbackA(300);
+			
+				}
+ 
+				if (direccion == 2)
+				{
+			
+					m.detenerC();
+					m.frontBbackA(400);
 				
-   			}
-			if( direccion == 5 ){
-				m.detenerA();
-				m.frontBbackC(650);
-						}
+				}
+         
+				if (direccion == 3)
+				{
+			
+					m.detenerC();
+					m.frontBbackA(600);
+				
+				}
+
+				if (direccion == 4)
+				{
+			
+					m.detenerC();
+					m.frontBbackA(800);
+				
+   				}
+			
+				if (direccion == 5)
+				{
+			
+					m.detenerA();
+					m.frontBbackC(650);
+			
+				}
         
-			if( direccion == 6 ){
-				m.detenerA();
-				m.frontCbackB(650);
-			}
+				if (direccion == 6)
+				{
+			
+					m.detenerA();
+					m.frontCbackB(650);
+			
+				}
  
-			if( direccion == 7 ){
-				m.detenerB();
-				m.frontCbackB(750);
-			}
+				if(direccion == 7)
+				{
+			
+					m.detenerB();
+					m.frontCbackB(750);
+			
+				}
          
-			if( direccion == 8 ){
-				m.detenerB();
-				m.frontAbackC(650);
+				if(direccion == 8)
+				{
+			
+					m.detenerB();
+					m.frontAbackC(650);
+			
+				}
+
+				if(direccion == 9)
+				{
+			
+					m.detenerC();
+					m.frontAbackB(500);
+			
+				}
+
 			}
 
-			if( direccion == 9 ){
-				m.detenerC();
-				m.frontAbackB(500);
-			}
-
-		
 		}
 
-		
-		}
     }
-}
-   
-   
-   
-   
 
+}
