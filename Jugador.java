165eddbad6f36a41bdd3fb80eb7eea609dc4d745
Franco (Choco) <gfdgeras@gmail.com>
2015-03-30 18:41:38 -0600

    Programa final beta arreglado 2.0
    Versión 2.0

--- Jugador.java
+++ Jugador.java
@@ -1,21 +1,31 @@
 //INTELIROBOT ESCUELA DE ROBÓTICA SLP
 //intelirobot.com.mx
 //Programador por Yoalli y Diego
-import lejos.nxt.addon.*;
-import lejos.nxt.*;
 
+//LIBRERIAS NECESARIAS.............
+import lejos.nxt.addon.*; 	//Para sensor infrarrojo y compas
+import lejos.util.*; // Para delay
+import lejos.nxt.*; // Para motores y sensores en general
+//..................................
+
+//CLASE PRINCIPAL JUGADOR....................
 public class Jugador
 {
-	
+	//MAIN...............................
 	public static void main(String[] args)
 	{
-		
+		//Se instancia el sensor de color puerto 2..........
 		ColorSensor color = new ColorSensor(SensorPort.S2);
+		//Se instancia el sensor de infrarrojo puerto 1..........
 		IRSeekerV2 infra = new IRSeekerV2(SensorPort.S1 ,IRSeekerV2.Mode.AC);
+		//Se instancia el sensor de compas puerto 3..........
 		CompassHTSensor brujula = new CompassHTSensor(SensorPort.S3);
+		//Se instancea el objeto m de la clase de movimientos
 		Movimientos m = new Movimientos();
- 		Pelota buscar = new Pelota(infra, compas, m, color);
+		//Se instancea el objeto buscar de la clase Pelota
+ 		Pelota buscar = new Pelota(infra, compas, m, color);//Se mandan los argumentos necesarios al constructor
 		
+		//Se manda llamar el metodo buscarPelota de la clase Pelota
 		buscar.buscaPelota();
 
 	}
--- Pelota.java
+++ Pelota.java
@@ -1,24 +1,29 @@
-import lejos.nxt.addon.*;
-import lejos.util.*;
-import lejos.nxt.*;
+//INTELIROBOT ESCUELA DE ROBÓTICA SLP
+//intelirobot.com.mx
+//Programador por Yoalli y Diego
 
+//LIBRERIAS NECESARIAS.............
+import lejos.nxt.addon.*; 	//Para sensor infrarrojo y compas
+import lejos.util.*; // Para delay
+import lejos.nxt.*; // Para motores y sensores en general
+//..................................
+
+//Clase Pelota...................................
 public class Pelota
 {
-    
+    //Declaraciones necesarias...............
+    //Sensor de color................
    	private ColorSensor color;
+   	//Sensor infrarrojo
     private IRSeekerV2 infra;
+    //Sensor de compas
     private CompassHTSensor brujula;
+    //Clase programada por YOLO para motores
     private Movimientos m;
+    //Atributos necesarios...............................
     private int direccion, orientacion;
     
-    public IRSeekerV2 getInfra()
-    { 
-    
-    	return infra;  
-
-    }
-    
-
+   //CONSTRUCTOR DE LA CLASE
     public Pelota(IRSeekerV2 infrarrojo, CompassHTSensor compas, Movimientos m, ColorSensor c)
     {
     
@@ -29,146 +34,103 @@ public class Pelota
 			 	  
     }
 
+    //Metodo para buscar la pelota...........................
     public void buscaPelota()
     {
-
-		
-		while(!Button.ESCAPE.isDown())
+		while(!Button.ESCAPE.isDown()) //mientras no se presione el boton ESCAPE
 		{
+			//SE RESETEA EL SENSOR COMPAS EN 0
+			brujula.resetCartesianZero();
 
+			//Imprimimos el id de color, IR, y grado de brujula
 			LCD.drawInt(color.getColorID(),1,1);
-			direccion = infra.getDirection();
+			direccion = infra.getDirection(); //la direccion es lo que detecta el sensor IR
 			LCD.drawInt(direccion, 1,3);
-			orientacion.resetCartesianZero();
+			orientacion=brujula.getDegreesCartesian();//la orientacion es el grado que detecta el compas
 			LCD.drawInt(orientacion, 1,5);
-			LCD.clearDisplay();
-			LCD.drawInt((int)sensor.getDegreesCartesian(),1,1);
-			Delay.msDelay(100);			
+			LCD.clearDisplay();//LIMPIAMOS LA PANTALLA
+			Delay.msDelay(100);//hacemos una pausa de 100 milisegundos			
+			//.........................
 
-			if (sensor.getDegreesCartesian() < 30)
-			{
-			
-				m.frontAbackB(800);
-			
-			}
-			
-			if (sensor.getDegreesCartesian() > 120)
-			{
-			
-				m.avanzarAC(800);
-			
-			}
-
-
+			//Si el sensor de color detecta un borde de la cancha
 			if (color.getColorID() == 1)
 		    {
-
-			    
-			    m.detenerAB();
+ 				m.detenerABC();
 			    m.retrocederAB(900);
-			    
-			    try
-			    {
-				
-					Thread.sleep(900);
-				
-				}
-				
-				catch (Exception e){}
-		    
+			    Delay.msDelay(900);
 		    }
 		    
+		    //Si el sensor de color detecta la cancha
 		    else
 		    {
-		    	if(sensor.getDegreesCartesian() > 300 && sensor.getDegreesCartesian()<30)
+		    	//Y si detecta que la inclinacion es mayor de 30 grados
+				if (orientacion > 30)
+				{
+					m.frontAbackB(800); //Que se acomode
+				}
+				//Y si detecta que la inclinacion es menor que 300 grados
+				if (orientacion < 300)
+				{
+					m.avanzarAC(800);//Se va a linear
+				}
+				//Y si esta acomodado.........
+		    	if(orientacion >= 300 && orientacion<=30)
 		    	{
-		    		m.avanzarAC(800);
-		    	}
-		    	else
-		    	{
-		    	if(sensor.getDegreesCartesian() < 30)
-		    	{
+		    		//DEPENDIENDO DEL IR SE VA A MOVER EL ROBOT
+					if (direccion == 1)
+					{
+						m.detenerB();
+						m.frontCbackA(300);
+					}
+	 
+					if (direccion == 2)
+					{
+						m.detenerC();
+						m.frontBbackA(400);
+					}
+	         
+					if (direccion == 3)
+					{
+						m.detenerC();
+						m.frontBbackA(600);
+					}
 
-            	  m.frontAbackB(800);
-            	}
-            
-            		m.detenerA();
-            		m.frontCbackB(900);
-            
-            	}
-		    
-				if (direccion == 1)
-				{
-			
-					m.detenerB();
-					m.frontCbackA(300);
-			
-				}
- 
-				if (direccion == 2)
-				{
-			
-					m.detenerC();
-					m.frontBbackA(400);
+					if (direccion == 4)
+					{
+						m.detenerC();
+						m.frontBbackA(800);
+	   				}
 				
-				}
-         
-				if (direccion == 3)
-				{
-			
-					m.detenerC();
-					m.frontBbackA(600);
-				
-				}
+					if (direccion == 5)
+					{
+						m.detenerA();
+						m.frontBbackC(650);
+					}
+	        
+					if (direccion == 6)
+					{
+						m.detenerA();
+						m.frontCbackB(650);
+					}
+	 
+					if(direccion == 7)
+					{
+						m.detenerB();
+						m.frontCbackB(750);
+					}
+	         
+					if(direccion == 8)
+					{
+						m.detenerB();
+						m.frontAbackC(650);
+					}
 
-				if (direccion == 4)
-				{
-			
-					m.detenerC();
-					m.frontBbackA(800);
-				
-   				}
-			
-				if (direccion == 5)
-				{
-			
-					m.detenerA();
-					m.frontBbackC(650);
-			
+					if(direccion == 9)
+					{
+						m.detenerC();
+						m.frontAbackB(500);
+					}
 				}
-        
-				if (direccion == 6)
-				{
-			
-					m.detenerA();
-					m.frontCbackB(650);
-			
-				}
- 
-				if(direccion == 7)
-				{
-			
-					m.detenerB();
-					m.frontCbackB(750);
-			
-				}
-         
-				if(direccion == 8)
-				{
-			
-					m.detenerB();
-					m.frontAbackC(650);
-			
-				}
-
-				if(direccion == 9)
-				{
-			
-					m.detenerC();
-					m.frontAbackB(500);
-			
-				}
-
 			}
 
 		}
